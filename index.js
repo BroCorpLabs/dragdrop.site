9f3ddefaa1a89a54f6f15cbcd19bccd8d144039d
prnthh <pranith_h@yahoo.com.sg>
2018-10-17 21:59:28 +0530

    Added features

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
 var express = require('express')
+
+//for file upload/storage
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'userSites/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({storage: storage})
+
 
 var app = express()
-
 app.use(express.static('static'))
-
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/static/dragdrop.html");
 })
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-})
-
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-  // req.files is array of `photos` files
-  // req.body will contain the text fields, if there were any
-})
-
-var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-app.post('/cool-profile', cpUpload, function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
-  //
-  // e.g.
-  //  req.files['avatar'][0] -> File
-  //  req.files['gallery'] -> Array
-  //
-  // req.body will contain the text fields, if there were any
-})
+app.post( '/upload', upload.single('dropfile'), function( req, res, next ) {
+  // Metadata about the uploaded file can now be found in req.file
+  console.log(req);
+  return res.status( 200 ).send('success');
+});
 
 var port=3000;
 
